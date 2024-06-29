496e764fc93ad7f5093a65cf53a4f960cbad3920
Simon Le Marchant <simon@marchantweb.com>
2024-06-29 15:45:02 -0400

    Updated package name

--- src/usePartyRef.ts
+++ src/usePartyRef.ts
@@ -1,9 +1,8 @@
 import {ref, Ref} from "vue";
 
 /**
- * A reactive ref that updates in real-time with other clients anywhere in the world.
- * No dev-ops required, `usePartyRef` uses websockets to sync data with minimal latency.
- * @docs https://github.com/marchantweb/use-party-ref
+ * A Vue 3 ref that syncs in real-time with other clients using PartyKit.
+ * @docs https://github.com/marchantweb/usePartyRef
  * @param value
  */
 export default function usePartyRef(value: any): Ref<any> {
