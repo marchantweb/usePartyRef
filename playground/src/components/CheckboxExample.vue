9c33fbccee504a3346948464161756654075b90b
Simon Le Marchant <simon@marchantweb.com>
2024-07-01 21:33:35 -0400

    chore: updated checkbox verbiage

--- playground/src/components/CheckboxExample.vue
+++ playground/src/components/CheckboxExample.vue
@@ -18,19 +18,19 @@ const totalChecked: ComputedRef<number> = computed(() => checkboxes.value.filter
     </div>
     <p><strong>Total checked boxes: {{ totalChecked }} / {{ checkboxes.length }} </strong></p>
     <p>
-      Everyone visiting this website can check/uncheck these checkboxes in real-time.
+      Everyone visiting this website can check/uncheck these checkboxes in real-time. If everyone leaves, they reset.
     </p>
   </div>
 </template>
 
 <style scoped>
-.checkboxes{
+.checkboxes {
   display: grid;
   grid-template-columns: repeat(10, 1fr);
   grid-gap: 1rem;
 }
 
-input[type="checkbox"]{
+input[type="checkbox"] {
   width: 1.3rem;
   height: 1.3rem;
 }
